e8aaaf1191d68f7a80ebb282fadcba56adabe83a
Abhimanyu Sharma <abhisea12@gmail.com>
2021-01-29 19:39:48 -0800

    Added an Extra Line

--- Basics/src/Template.java
+++ Basics/src/Template.java
@@ -1,5 +1,6 @@
 public class Template {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("Do an hour of coding every day");
     }
 }
